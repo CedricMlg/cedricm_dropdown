571b5f665cdb13168fa3ab33e096b64e5809bdc4
CedricMlg <malingre.cedric@gmail.com>
2023-03-16 17:17:52 +0100

    Update dropdown to get the selected value

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -1,18 +1,26 @@
 import React from "react";
-import "../../styles.css"
+import "../../styles.css";
 
 /**
  * It's a function that takes in a label and an array of options, and returns a dropdown menu with the
  * label as the title and the options as the options
  */
-export const Dropdown = ({ label, options }) => {
+export const Dropdown = ({ label, options, onChange }) => {
+  const handleChange = (e) => {
+    onChange(e.target.value);
+  };
+
   return (
     <div className="dropdown">
       {" "}
       <label hmtlfor={label}>{label}</label>
-      <select name={label} id={label}>
+      <select onChange={handleChange} name={label} id={label}>
         {options.map((opt, index) => {
-          return <option key={index} value={opt.value}>{opt.name}</option>;
+          return (
+            <option key={index} value={opt.value}>
+              {opt.name}
+            </option>
+          );
         })}
       </select>
     </div>
